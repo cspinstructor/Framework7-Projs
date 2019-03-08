08d765f21d78c1f978e5e285ced8b8e3b9b8203d
paulchin <paulchin20@yahoo.com>
2019-03-08 17:53:44 +0800

    calender + news

--- F7-HanchiangApp/my-app.js
+++ F7-HanchiangApp/my-app.js
@@ -15,14 +15,16 @@ $$(document).on('deviceready', function() {
   console.log('Device is ready!');
 });
 
-// Now we need to run the code that will be executed only for About page.
-
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function(page) {
+myApp.onPageInit('index', function(page) {
+  getHomePage();
+});
+
+myApp.onPageInit('timetable', function(page) {
   // Do something here for "about" page
 });
 
-myApp.onPageInit('test', function(page) {
+myApp.onPageInit('news', function(page) {
   // Do something here for "about" page
 });
 
@@ -43,10 +45,87 @@ $$(document).on('pageInit', '.page[data-page="about"]', function(e) {
   //myApp.alert('Here comes About page 2');
 });
 
-$$('#id-about').on('click', function() {
-  console.log('id-about');
+$$('#id-home').on('click', function() {
+  getHomePage();
+});
+
+$$('#id-calendar').on('click', function() {
+  getCalendars();
+});
+
+$$('#id-news').on('click', function() {
+  console.log('id-news');
+  getNews();
 });
 
 $$('.panel-left').on('panel:open', function() {
   console.log('Panel left: open');
 });
+
+//--- Hanchiang News ---
+function getNews() {
+  var newsContent = '';
+  //const apiRoot = 'https://hjuapp.site/wp-json';
+  const apiRoot = 'http://www.hanchiangnews.com/en/wp-json';
+  var wp = new WPAPI({ endpoint: apiRoot });
+  wp.posts()
+    .perPage(3)
+    .then(function(posts) {
+      posts.forEach(function(post) {
+        console.log(post.content.rendered);
+        newsContent += post.content.rendered;
+      });
+      $$('#id-news-content').html(newsContent);
+    });
+}
+
+//--- Hanchiang Calendar ---
+function getCalendars() {
+  var calendarContent = '';
+  const apiRoot = 'https://hjuapp.site/wp-json';
+
+  var wp = new WPAPI({ endpoint: apiRoot });
+
+  wp.posts()
+    .categories(6)
+    .then(function(posts) {
+      posts.forEach(function(post) {
+        console.log(post.content.rendered);
+        calendarContent += post.content.rendered;
+      });
+      $$('#id-calendar-content').html(calendarContent);
+    });
+}
+
+//--- Hanchiang Home Page ---
+function getHomePage() {
+  var content = '';
+  const apiRoot = 'http://hcu.edu.my/wp-json';
+  // http://hcu.edu.my/wp-json/wp/v2/pages/7765/
+  var wp = new WPAPI({ endpoint: apiRoot });
+
+  // wp.pages()
+  //   .id(7765)
+  //   .then(function(posts) {
+  //     console.log(posts);
+  //     posts.forEach(function(post) {
+  //       console.log(post.content.rendered);
+  //       content += post.content.rendered;
+  //     });
+  //     $$('#id-home-content').html(content);
+  //   });
+
+  wp.pages()
+    .id(7765)
+    .then(function(pages) {
+      content = pages.content.rendered;
+      console.log(content);
+      $$('#id-home-content').html(content);
+      // pages.forEach(function(page) {
+      //   console.log(page.content.rendered);
+      //   content += page.content.rendered;
+      // });
+      // console.log(content);
+      // $$('#id-home-content').html(content);
+    });
+}
